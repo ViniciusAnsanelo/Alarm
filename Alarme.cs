6effad436b6cacdf89cb95aa0d0454b602f6f8c5
vinic <vinic@VINCIUS>
2023-03-10 16:19:51 -0300

    Adicionar arquivos de projeto.

---
+++ Alarme.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Alarm
+{
+    public class Alarme
+    {
+        //construtor
+        public Alarme()
+        {
+            this.time = 3;
+        }
+
+        public Alarme(int time)
+        {
+            this.time = time;
+        }
+
+        //atributos 
+
+        private int time;
+
+        public int Time
+        {
+            get { return this.time; }
+            set { this.time = 60000 * value; }
+        }
+
+        public void Start()
+        {
+            //to work must add system.Threading;
+            Thread.Sleep(this.Time);
+            //define frequency and duration of the sound;
+            Console.Beep(250, 10);
+        }
+
+
+    }
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alarm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alarme alarme = new Alarme();
+            Console.WriteLine("Pitch Time");
+            Console.Write("Report pitch duration: ");
+            alarme.Time = Convert.ToInt32(Console.ReadLine());
+            alarme.Time = 
+
+
+
+
+        }
+    }
+}
